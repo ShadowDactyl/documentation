53ddfda2858f06ede438dcba5d78605a5e704f7b
ShadowDactyl <shadowcraftofficial272@gmail.com>
2026-05-22 01:43:35 +0400

    Fix case sensitivity in GitHub repository URL

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:shadowdactyl/documentation.git master:gh-pages
+git push -f git@github.com:ShadowDactyl/documentation.git master:gh-pages
 
 cd -
